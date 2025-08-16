bc4e0dc68d184f504ff023c1a428210c101349fa
nsmultani <navjotsingh5@academic.rrc.ca>
2025-08-16 16:01:30 +0530

    Configure ActiveAdmin Customer resource with image support

--- app/admin/customers.rb
+++ app/admin/customers.rb
@@ -1,18 +1,41 @@
 ActiveAdmin.register Customer do
+  permit_params :full_name, :phone_number, :email_address, :notes, :image
 
-  # See permitted parameters documentation:
-  # https://github.com/activeadmin/activeadmin/blob/master/docs/2-resource-customization.md#setting-up-strong-parameters
-  #
-  # Uncomment all parameters which should be permitted for assignment
-  #
-  # permit_params :full_name, :phone_number, :email_address, :notes
-  #
-  # or
-  #
-  # permit_params do
-  #   permitted = [:full_name, :phone_number, :email_address, :notes]
-  #   permitted << :other if params[:action] == 'create' && current_user.admin?
-  #   permitted
-  # end
-  
+  index do
+    selectable_column
+    id_column
+    column :full_name
+    column :phone_number
+    column :email_address
+    column :notes
+    column :created_at
+    actions
+  end
+
+  form do |f|
+    f.inputs do
+      f.input :full_name
+      f.input :phone_number
+      f.input :email_address
+      f.input :notes
+      f.input :image, as: :file
+    end
+    f.actions
+  end
+
+  show do
+    attributes_table do
+      row :full_name
+      row :phone_number
+      row :email_address
+      row :notes
+      row :image do |customer|
+        if customer.image.attached?
+          image_tag customer.image, style: "max-width: 200px;"
+        end
+      end
+      row :created_at
+      row :updated_at
+    end
+  end
 end
