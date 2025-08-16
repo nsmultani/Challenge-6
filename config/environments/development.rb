ba418b9dc5c07e60ee03df99bd022d24985d5882
nsmultani <navjotsingh5@academic.rrc.ca>
2025-08-16 16:55:02 +0530

    Complete Rails CRM challenge with customer controller, views, and routing

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -18,7 +18,7 @@ Rails.application.configure do
   # Enable/disable Action Controller caching. By default Action Controller caching is disabled.
   # Run rails dev:cache to toggle Action Controller caching.
   if Rails.root.join("tmp/caching-dev.txt").exist?
-    config.action_controller.perform_caching = true
+    config.action_controller.perform_caching = false
     config.action_controller.enable_fragment_cache_logging = true
     config.public_file_server.headers = { "cache-control" => "public, max-age=#{2.days.to_i}" }
   else
@@ -26,8 +26,10 @@ Rails.application.configure do
   end
 
   # Change to :null_store to avoid any caching.
-  config.cache_store = :memory_store
+  config.cache_store = :null_store
 
+  config.assets.debug = true
+  config.assets.digest = false
   # Store uploaded files on the local file system (see config/storage.yml for options).
   config.active_storage.service = :local
 
