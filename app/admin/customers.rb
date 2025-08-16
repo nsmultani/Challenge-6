92874e785b9b327c84e45a9617acbe62acb99dc5
nsmultani <navjotsingh5@academic.rrc.ca>
2025-08-16 16:03:31 +0530

    Add ActiveStorage and Customer model validations

--- app/admin/customers.rb
+++ app/admin/customers.rb
@@ -39,3 +39,11 @@ ActiveAdmin.register Customer do
     end
   end
 end
+
+class Customer < ApplicationRecord
+  has_one_attached :image
+  
+  validates :full_name, presence: true
+  validates :phone_number, presence: true
+  validates :email_address, format: { with: URI::MailTo::EMAIL_REGEXP }, allow_blank: true
+end
